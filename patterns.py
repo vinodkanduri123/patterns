5050423c4b8f36374978fcccc6fb4b6f47668c4c
vinodkanduri123 <133983884+vinodkanduri123@users.noreply.github.com>
2023-06-16 13:19:54 +0530

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,217 @@
+
+'''# star pattern squre
+#1)
+n=int(input("Enter which pattern you want:"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print("*",end='')
+    print()'''
+
+
+
+'''
+#2)
+n=int(input("Enter which pattern you want:"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(i,end='')
+    print()'''
+'''
+#3)
+n=int(input("Enter which pattern you want:"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(j,end='')
+    print()'''
+
+'''
+#4)
+n=int(input("Enter which pattern you want:"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(n+1-i,end='')
+    print()'''
+
+
+'''
+#5)
+n=int(input("Enter which pattern you want:"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(n+1-j,end='')
+    print()'''
+
+
+
+'''
+#6)
+n=int(input("Enter which pattern you want:"))
+for i in range(1,n+1):
+    for j in range(i):
+        print('*',end='')
+    print()'''
+
+
+'''
+#7)
+n=int(input("Enter which pattern you want:"))
+for i in range(1,n+1):
+    for j in range(i):
+        print(i,end='')
+    print()'''
+
+'''
+#8)
+n=int(input("Enter which pattern you want:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end='')
+    print()'''
+
+'''
+#9)
+n=int(input("Enter which pattern you want:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(n+1-j,end='')
+    print()'''
+
+
+'''
+#10)
+
+n=int(input("Enter which pattern you want:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(n+1-i,end='')
+    print()
+'''
+
+
+'''
+#11)
+n=int(input("Enter the number of rows: ")) 
+for i in range(1,n+1): 
+    print(" "*(n-i)," *"*(i-1)) '''
+
+
+#12)
+'''
+n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(chr(96+i),end='')
+    print()'''
+
+#13)
+'''n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(chr(96+j),end='')
+    print()'''
+
+
+#14)
+'''n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(96+i),end='')
+    print()'''
+
+
+#15)
+'''
+n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(96+j),end='')
+    print()
+'''
+
+#16)
+'''n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(chr(64+i),end='')
+    print()'''
+
+#17)
+'''
+n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(chr(96+n+1-i),end='')
+    print()'''
+
+'''#18)
+n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(chr(96+n+1-i),end='')
+    print()'''
+
+#19)
+
+'''n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(96+n+1-i),end='')
+    print()'''
+
+#20)
+'''
+n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(96+n+1-j),end='')
+    print()'''
+
+
+'''
+#21)
+n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(chr(64+i),end='')
+    print()'''
+
+#22)
+'''
+n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(chr(64+j),end='')
+    print()'''
+
+
+#23)
+
+
+
+'''n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(64+j),end='')
+    print()'''
+
+#24)
+'''n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(64+i),end='')
+    print()'''
+
+#25)
+'''n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(64+n+1-j),end='')
+    print()'''
+
+#26)
+'''
+n=int(input("Enter number of rows you want:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(64+n+1-i),end='')
+    print()'''
